871662748f5c6d913597d30dabc9a5057612a619
Gary1q2 <gary1q2@hotmail.com>
2020-06-18 20:39:35 +1000

    Initialising lobby class

---
+++ lobby.js
@@ -0,0 +1,36 @@
+module.exports = class Lobby {
+    constructor() {
+        this.players = {};
+        this.messages = [];
+    }
+
+    /* A new player joined the lobby
+    */
+    playerJoin() {
+
+    }
+
+    /* A player left the lobby
+    */
+    playerLeave() {
+
+    }
+
+    /* System broadcasted a message
+    */
+    systemMsg() {
+
+    }
+
+    /* Player sent a message
+    */
+    playerMsg() {
+
+    }
+
+    /* Player started a game
+    */
+    playerPlay() {
+        
+    }
+}
